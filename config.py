5f9d25fe5dae0daf0dc8affee6347c0fe5e846a0
Dykleigh <202380181@psu.palawan.edu.ph>
2025-12-14 22:07:28 +0800

    added config for testing

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 
 class Config:
     MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'password')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'root123')
     MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
-    MYSQL_DB = os.getenv('MYSQL_DB', 'mydatabase')
+    MYSQL_DB = os.getenv('MYSQL_DB', 'hanz_sales_db')
     MYSQL_PORT = int(os.getenv('MYSQL_PORT', 3306))
     MYSQL_POOL_NAME = os.getenv('MYSQL_POOL_NAME', 'mypool')
     MYSQL_POOL_SIZE = int(os.getenv('MYSQL_POOL_SIZE', 5))
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
     API_USERNAME = os.getenv('API_USERNAME', 'admin')
-    API_PASSWORD = os.getenv('API_PASSWORD', 'adminpassword')
+    API_PASSWORD = os.getenv('API_PASSWORD', 'password')
