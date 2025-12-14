f4760a87e85433f38dbfde895a6d4a9bc49494c6
Dykleigh <202380181@psu.palawan.edu.ph>
2025-12-14 19:34:31 +0800

    project scaffolding and uploaded requirements.txt

---
+++ config.py
@@ -0,0 +1,13 @@
+import os 
+
+class Config:
+    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'password')
+    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
+    MYSQL_DB = os.getenv('MYSQL_DB', 'mydatabase')
+    MYSQL_PORT = int(os.getenv('MYSQL_PORT', 3306))
+    MYSQL_POOL_NAME = os.getenv('MYSQL_POOL_NAME', 'mypool')
+    MYSQL_POOL_SIZE = int(os.getenv('MYSQL_POOL_SIZE', 5))
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
+    API_USERNAME = os.getenv('API_USERNAME', 'admin')
+    API_PASSWORD = os.getenv('API_PASSWORD', 'adminpassword')
